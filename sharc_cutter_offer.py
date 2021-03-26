6d29d4be30143c6633be38d5c61e74b3fe16e254
Joe Cooper <josephcharlescooper@gmail.com>
2021-03-26 17:02:20 +0000

    comments

--- sharc_cutter_offer.py
+++ sharc_cutter_offer.py
@@ -8,38 +8,48 @@
 # 
 # use command 
 # for i in `ls -d */`; do cd $i ; ./thisscript.py ; $SHARC/data_extractor.x newoutput.dat ; cd .. ; done 
+#
+# this requires that you've already run $SHARC/data_extractor.x - if you haven't, do, or uncomment the first os.system command
+# this will print the changed output to files called newoutput.dat and newoutput.xyz - imaginatively corresponding to output.dat and output.xyz respectively
+#
+# according to the source code of SHARC (from mar 21), the output.dat file is the only file that is needed to generate the data - the .lis and .log files are purely for humans
+#
+#
 
 import os
 import numpy as np
 
+#os.system("$SHARC/data_extractor.x output.dat")
+
 energy_threshold = 0.15 # eV - the smallest allowed energy between occupied and ground state
 
-no_atoms=9
+no_atoms=9  # I assume this is fairly self-explanatory... change to number of atoms of your molecule
 
-step_time = 0.5
+step_time = 0.5 # fs - fairly standard value, but change if needed.
 
 
-energy_file="output_data/energy.out"
+energy_file="output_data/energy.out" #  the spin data for each step
 
-spin_file="output_data/spin.out"
+spin_file="output_data/spin.out" # the spin data for each step
 
-energy_data=np.genfromtxt(energy_file, skip_header=3)
+energy_data=np.genfromtxt(energy_file, skip_header=3) # parse data
 
-spin_data=np.genfromtxt(spin_file, skip_header=3)
+spin_data=np.genfromtxt(spin_file, skip_header=3) # parse data
 
-print("Threshold = "+str(energy_threshold)+" eV")
 
-for i in range(np.shape(energy_data)[0]):
-    state_number = np.argmax(spin_data[i,2:]<1)+1 # The state to which we cannot get close
-    if energy_data[i,2]-energy_data[i,state_number+3] < energy_threshold:
-        print("Time that energy is below threshold = " + str(energy_data[i-1,0])+" fs")
-        time_break=energy_data[i,0]
+print("Threshold = "+str(energy_threshold)+" eV") # for checking...
+
+for i in range(np.shape(energy_data)[0]): # for each time step
+    state_number = np.argmax(spin_data[i,2:]<1)+1 # The state to which we cannot get close - this finds the lowest energy state of 2S less than 1 - i.e. S_0
+    if energy_data[i,2]-energy_data[i,state_number+3] < energy_threshold: # if the current potential energy (i.e. the current state's energy) is closer than energy_threshold eV away from S_0
+        print("Time that energy is below threshold = " + str(energy_data[i-1,0])+" fs") # print time
+        time_break=energy_data[i,0] # time is gets to that condition - this is the FIRST timestep we DON'T want
         break
     
 
-step_number=int(time_break/step_time + 1)
+step_number=int(time_break/step_time + 1) # the step number of that state
 
 
-os.system("k=`grep -n  '! 0 Step' output.dat | head -n"+ str(step_number) +" | tail -n1 | cut -d':' -f1` ; head -n`expr $k - 1` output.dat > newoutput.dat")
-os.system("head -n"+str((step_number-1)*(no_atoms+2))+" output.xyz > newoutput.xyz")
-#os.system("$SHARC/data_extractor.x newoutput.dat")
+os.system("k=`grep -n  '! 0 Step' output.dat | head -n"+ str(step_number) +" | tail -n1 | cut -d':' -f1` ; head -n`expr $k - 1` output.dat > newoutput.dat") # finds the point in the .dat file whence we don't want, and deletes all lines after
+os.system("head -n"+str((step_number-1)*(no_atoms+2))+" output.xyz > newoutput.xyz") # deletes all geometries we don't want
+#os.system("$SHARC/data_extractor.x newoutput.dat") # uncomment if you want to run the command from this script
